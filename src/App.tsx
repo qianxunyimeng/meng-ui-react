a25ac038bdcc91a21505af873f7d158c360ea9cf
shiqingliang <460960620@qq.com>
2022-08-12 01:06:55 +0800

    完成button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Button, { ButtonType,ButtonSize} from './components/Button/button';
 
 function App() {
-  const a = '123'
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world</h1>
+      <code>
+        const a = '123'
+      </code>
+      <Button>确定</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large} onClick={() => { alert('123')}}>确定</Button>
+      <Button btnType={ ButtonType.Link} href="https://www.baidu.com" disabled>确定</Button>
     </div>
   );
 }
