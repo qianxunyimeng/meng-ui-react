dc1b74a33cb57b6f86472e0ddb070ad6c62c602f
shiqingliang <460960620@qq.com>
2022-09-04 20:40:24 +0800

    完成组件库

--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -5,13 +5,11 @@ import Icon from '../Icon/icon'
 import Transition from '../Transition/transition'
 import useDebounce from '../../hooks/useDebounce'
 import useClickOutside from '../../hooks/useClickOutside'
-
-
 interface DataSourceObject {
   value: string;
 }
-export type DataSourceType<T = {}> = (T & DataSourceObject)
-export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
+export type DataSourceType<T = {}> = T & DataSourceObject
+export interface AutoCompleteProps extends Omit<InputProps, 'onSelect' | 'onChange'> {
   /**
    * 返回输入建议的方法，可以拿到当前的输入，然后返回同步的数组或者是异步的 Promise
    * type DataSourceType<T = {}> = T & DataSourceObject
@@ -20,128 +18,150 @@ export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
   /** 点击选中建议项时触发的回调*/
   onSelect?: (item: DataSourceType) => void;
   /** 文本框发生改变的时候触发的事件*/
-  //onChange?: (value: string) => void;
+  onChange?: (value: string) => void;
   /**支持自定义渲染下拉项，返回 ReactElement */
   renderOption?: (item: DataSourceType) => ReactElement;
 }
 
-export const AutoComplete: FC<AutoCompleteProps> = (props) => { 
-
-  const { 
+/**
+ * 输入框自动完成功能。当输入值需要自动完成时使用，支持同步和异步两种方式
+ * 支持 Input 组件的所有属性 支持键盘事件选择
+ * ### 引用方法
+ * 
+ * ~~~js
+ * import { AutoComplete } from 'vikingship'
+ * ~~~
+ */
+export const AutoComplete: FC<AutoCompleteProps> = (props) => {
+  const {
     fetchSuggestions,
     onSelect,
+    onChange,
     value,
     renderOption,
     ...restProps
   } = props
 
-  const [inputValue, setInputValue] = useState(value as string)
-  
-  const [suggestions, setSuggestions] = useState<DataSourceType[]>([])
-  const [loading, setLoading] = useState(false)
-  const [highlightIndex,setHighlightIndex] = useState(-1)
+  const [ inputValue, setInputValue ] = useState(value as string)
+  const [ suggestions, setSugestions ] = useState<DataSourceType[]>([])
+  const [ loading, setLoading ] = useState(false)
+  const [ showDropdown, setShowDropdown] = useState(false)
+  const [ highlightIndex, setHighlightIndex] = useState(-1)
   const triggerSearch = useRef(false)
   const componentRef = useRef<HTMLDivElement>(null)
-  const debouncedValue = useDebounce(inputValue, 500)
-  useClickOutside(componentRef,() => {
-    setSuggestions([])
-  })
+  const debouncedValue = useDebounce(inputValue, 300)
+  useClickOutside(componentRef, () => { setSugestions([])})
   useEffect(() => {
     if (debouncedValue && triggerSearch.current) {
+      setSugestions([])
       const results = fetchSuggestions(debouncedValue)
-      if (results instanceof Promise) { 
-        console.log("triggered");
+      if (results instanceof Promise) {
         setLoading(true)
-        results.then(data => { 
+        results.then(data => {
           setLoading(false)
-          setSuggestions(data)
+          setSugestions(data)
+          if (data.length > 0) {
+            setShowDropdown(true)
+          }
         })
       } else {
-        setSuggestions(results)       
+        setSugestions(results)
+        setShowDropdown(true)
+        if (results.length > 0) {
+          setShowDropdown(true)
+        } 
       }
     } else {
-      setSuggestions([])
+      setShowDropdown(false)
     }
     setHighlightIndex(-1)
-  }, [debouncedValue])
-
+  }, [debouncedValue, fetchSuggestions])
   const highlight = (index: number) => {
     if (index < 0) index = 0
     if (index >= suggestions.length) {
       index = suggestions.length - 1
     }
-    console.log(index);
-    
     setHighlightIndex(index)
   }
-
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    console.log(e.keyCode);
-    
-    switch (e.keyCode) {
-      case 13://回车
-        if (suggestions[highlightIndex]) { 
+    switch(e.keyCode) {
+      case 13:
+        if (suggestions[highlightIndex]) {
           handleSelect(suggestions[highlightIndex])
         }
         break
-      case 38: //方向键 up
+      case 38:
         highlight(highlightIndex - 1)
         break
-      case 40: //方向键 down
-        highlight(highlightIndex + 1)       
+      case 40:
+        highlight(highlightIndex + 1)
         break
-      case 27:// esc
-        setSuggestions([])
+      case 27:
+        setShowDropdown(false)
         break
       default:
         break
     }
   }
-  
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.trim()
+    console.log('triggered the value', value)
     setInputValue(value)
+    if (onChange) {
+      onChange(value)
+    }
     triggerSearch.current = true
   }
   const handleSelect = (item: DataSourceType) => {
     setInputValue(item.value)
-    setSuggestions([])
+    setShowDropdown(false)
     if (onSelect) {
       onSelect(item)
     }
     triggerSearch.current = false
   }
-
-  const renderTemplate = (item:DataSourceType) => {
+  const renderTemplate = (item: DataSourceType) => {
     return renderOption ? renderOption(item) : item.value
   }
-
   const generateDropdown = () => {
     return (
-      <ul className='mx-suggestion-list'>
-        {
-          suggestions?.map((item, index) => { 
+      <Transition
+        in={showDropdown || loading}
+        animation="zoom-in-top"
+        timeout={300}
+        onExited={() => {setSugestions([])}}
+      >
+        <ul className="mx-suggestion-list">
+          { loading &&
+            <div className="suggstions-loading-icon">
+              <Icon icon="spinner" spin/>
+            </div>
+          }
+          {suggestions.map((item, index) => {
             const cnames = classNames('suggestion-item', {
               'is-active': index === highlightIndex
             })
             return (
-              <li key={index} className={ cnames } onClick={ () => handleSelect(item)}>
-                { renderTemplate(item) }
+              <li key={index} className={cnames} onClick={() => handleSelect(item)}>
+                {renderTemplate(item)}
               </li>
             )
-          })
-        }
-      </ul>
+          })}
+        </ul>
+      </Transition>
     )
   }
-
-
-  
   return (
-    <div className="mx-auto-complete" ref={ componentRef }>
-      <Input value={inputValue} onChange={handleChange} onKeyDown={ handleKeyDown } {...restProps}></Input>
-      { loading && <ul><Icon icon="spinner" spin></Icon></ul>}
-      {(suggestions?.length > 0) && generateDropdown()}
+    <div className="mx-auto-complete" ref={componentRef}>
+      <Input
+        {...restProps}
+        value={inputValue}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+      {generateDropdown()}
     </div>
   )
 }
+
+export default AutoComplete;
---
+++ src/components/Upload/upload.stories.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import Upload, { UploadFile } from "./upload";
+import Button from "../Button/button"
+
+const defaultFileList: UploadFile[] = [
+  {uid: "101",size: 1234,name: "hello.md",status:"uploading",percent:30},
+  {uid: "102",size: 1234,name: "xyz.md",status:"success",percent:30},
+  {uid: "103",size: 1234,name: "abc.md",status:"error",percent:30},
+
+]
+
+
+const checkFileSize = (file: File) => {
+  if (Math.round(file.size / 1024) > 50) {// 大于50K
+    alert("file too larger")
+    return false;
+  }
+  return true
+}
+
+const filePromise = (file: File) => {
+  const newFile = new File([file], "new_name.txt", { type: file.type })
+  return Promise.resolve(newFile)
+}
+
+const SimpleUpload = () => {
+  return (
+    //https://jsonplaceholder.typicode.com/posts/
+    // https://www.mocky.io/v2/5cc8019d300000980a055e76
+    <Upload
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      onProgress={action('progress')}
+      onSuccess={action('success')}
+      onError={action("error")}
+      onChange={action('changed')}
+      //beforeUpload={filePromise}
+      defaultFileList={defaultFileList}
+      name="filename"
+      data={{ 'key': 'value' }}
+      headers={{ 'x-pb': "meng" }}
+      accept=".jpg"
+      multiple
+      drag
+    >
+      {/* <Button size="lg" btnType="primary">点击上传</Button> */}
+
+    </Upload>
+  )
+}
+
+storiesOf("Upload", module)
+.add("Upload",SimpleUpload)
